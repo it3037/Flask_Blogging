f06a0a891ef23251cb8a0affa6d9f9c131f516f9
it3037rakesh <rakeshpal51196@gmail.com>
2021-10-15 08:42:46 +0530

    templates loaded

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
+
+
+app.config['SECRET_KEY']='63e274cf0a19aaef0d401369cde77b52'
 
 posts = [
   {
@@ -17,12 +21,26 @@ posts = [
 ]
 
 @app.route('/')
-def index():
+def home():
   return render_template('home.html',posts=posts)
 
 @app.route('/about')
 def about():
   return render_template('about.html',title="About")
+
+
+@app.route('/register',methods=['POST','GET'])
+def register():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.username.data}!','success')
+    return redirect(url_for('home'))
+  return render_template('register.html',title="Register",form=form)
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html',title="Login",form=form)
 
 if __name__ == '__main__':
   app.run(debug=True)
