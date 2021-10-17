74dacaeb0d7ffba1842503bdacf790bef7e4b3e3
it3037rakesh <rakeshpal51196@gmail.com>
2021-10-17 07:45:58 +0530

    Debugging

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -4,7 +4,7 @@ from flask_bcrypt import Bcrypt
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='63e274cf0a19aaef0d401369cde77b52'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/rakesh/Desktop/Flaskkk/Flask_Blogging/flaskblog/site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired, Email,Length,EqualTo
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
   username=StringField('Username', validators=[DataRequired(),Length(min=2,max=20)])
@@ -8,6 +9,11 @@ class RegistrationForm(FlaskForm):
   password=PasswordField('Password', validators=[DataRequired()])
   confirm_password=PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
   submit=SubmitField('Sign Up')
+
+
+  def validate_username(self,username):
+    if True:
+      raise ValidationError('Validation message')
 
 
 class LoginForm(FlaskForm):
