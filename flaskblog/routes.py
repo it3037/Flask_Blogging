8240f626f808d5bc4392096ffbd1772a7f8ddb2c
it3037rakesh <rakeshpal51196@gmail.com>
2021-10-22 06:16:19 +0530

    changes done

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,7 @@
 import  os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect,request
+from flask import render_template, url_for, flash, redirect,request, abort
 from flaskblog import app , db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
@@ -98,4 +98,40 @@ def new_post():
     db.session.commit()
     flash('Your post has been created!', 'success')
     return redirect(url_for('home'))
-  return render_template('create_post.html', title='New post', form=form)
+  return render_template('create_post.html', title='New post', form=form, legend='New post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+  post = Post.query.get_or_404(post_id)
+  return render_template('post.html', title=post.title,post=post)
+
+@app.route("/post/<int:post_id>/update", methods=["POST","GET"])
+@login_required
+def update_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+    abort(403)
+  form = PostForm()
+  if form.validate_on_submit():
+    post.title = form.title.data
+    post.content = form.content.data
+    db.session.commit()
+    flash('Your post has been updated!', 'success')
+    return redirect(url_for('post',post_id=post.id))
+  elif request.method == 'GET':
+
+    form.title.data = post.title
+    form.content.data = post.content
+  return render_template('create_post.html', title="Update post", form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
